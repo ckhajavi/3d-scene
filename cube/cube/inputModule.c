0def18c7dc7d84beea1e23919b0512c15684c52b
ckhajavi <cara.khajavi@gmail.com>
2016-11-30 18:23:14 -0800

    fixing my_GL_look_at

--- cube/cube/inputModule.c
+++ cube/cube/inputModule.c
@@ -131,24 +131,21 @@ void mouseMoveHandler( int x, int y ){
   glutPostRedisplay( );
 }
 
-void mouseClickHandler(int x, int y){
-    
-}
 float vecLength(float x, float y, float z){
     return sqrtf(powf(x,2) + powf(y,2) + powf(z,2));
     
 }
 
 void normalizeVector(float vec[]){
-    float arr[3];
+    float arr[3] = {0,0,0};
     
     for (int i = 0; i < 3; ++i){
         arr[i] = vec[i];
+        printf("%f", arr[i]);
     }
     
     float len = vecLength(vec[0], vec[1], vec[2]);
     printf("%f", len);
-    printf("%f", sizeof(vec));
     for (int i = 0; i < 3; ++i) {
         vec[i] = arr[i]/len;
         printf(" ");
@@ -156,58 +153,107 @@ void normalizeVector(float vec[]){
     }
 }
 
-float* crossProduct(float *a, float *b)
+GLfloat* crossProduct(GLfloat *a, GLfloat *b)
 {
-    float Product[3];
+    //malloc(sizeof(int) * size);
+    float *Product = malloc(sizeof(float) *3);
     
     //Cross product formula
     Product[0] = (a[1] * b[2]) - (a[2] * b[1]);
     Product[1] = (a[2] * b[0]) - (a[0] * b[2]);
     Product[2] = (a[0] * b[1]) - (a[1] * b[0]);
-    
+    for (int i = 0; i< 3; ++i) {
+        printf(" ");
+        printf("%f", Product[i]);
+    }
+ 
     return Product;
+}
+
+//GLfloat** multMat(GLfloat **a, GLfloat **b)
+//{
+//    int i, j, k;
+//    
+//    GLfloat *arr[4];
+//    for (i=0; i<4; i++)
+//        arr[i] = (GLfloat *)malloc(4 * sizeof(GLfloat));
+//    int sum = 0;
+//    //Multiplication Logic
+//    for (i = 0; i < 4; i++) {
+//        for (j = 0; j < 4; j++) {
+//            sum = 0;
+//            for (k = 0; k < 4; k++) {
+//                sum = sum + a[i][k] * b[k][j];
+//            }
+//            arr[i][j] = sum;
+//        }
+//    }
+//    return arr;
+//}
+
+void multMat(GLfloat a[][4], GLfloat b[][4], GLfloat arr[][4])
+{
+    // Initializing elements of matrix mult to 0.
+    for(int i = 0; i < 4; ++i)
+        for(int j = 0; j < 4; ++j)
+        {
+            arr[i][j]=0;
+        }
+    
+    // Multiplying matrix a and b and storing in array mult.
+    for(int i = 0; i < 4; ++i)
+        for(int j = 0; j < 4; ++j)
+            for(int k = 0; k < 4; ++k)
+            {
+                arr[i][j] += a[i][k] * b[k][j];
+            }
 }
 
 void my_GL_lookAt(float eyeX, float eyeY, float eyeZ, float atX, float atY, float atZ, float upX, float upY, float upZ){
     
-    float f[3] = { atX - eyeX, atY - eyeY, atZ - eyeZ };
+    GLfloat f[3] = { atX - eyeX, atY - eyeY, atZ - eyeZ };
     float up[3] = {upX, upY, upZ};
-    GLfloat mat[16];
+    GLfloat mat[4][4];
+
     //normatlize f
     printf("%lu", sizeof(f));
     normalizeVector(f);
     float *side = crossProduct(f, up);
+
     normalizeVector(side);
     
     //Recompute up as: up = side x forward
     float *up_normalized = crossProduct(side, f);
     normalizeVector(up_normalized);
     
-    //------------------
-    mat[0] = side[0];
-    mat[4] = side[1];
-    mat[8] = side[2];
-    mat[12] = 0.0;
-    //------------------
-    mat[1] = up[0];
-    mat[5] = up[1];
-    mat[9] = up[2];
-    mat[13] = 0.0;
-    //------------------
-    mat[2] = -f[0];
-    mat[6] = -f[1];
-    mat[10] = -f[2];
-    mat[14] = 0.0;
-    //------------------
-    mat[3] = mat[7] = mat[11] = 0.0;
-    mat[15] = 1.0;
+    mat[0][0] = side[0];
+    mat[1][0] = side[1];
+    mat[2][0] = side[2];
+    mat[3][0] = 0.0;
     
+    //
+    mat[0][1] = up_normalized[0];
+    mat[1][1] = up_normalized[1];
+    mat[2][1] = up_normalized[2];
+    mat[3][1] = 0.0;
+    
+    //
+    mat[0][2] = -f[0];
+    mat[1][2] = -f[1];
+    mat[2][2] = -f[2];
+    mat[3][2] = 0.0;
+    
+    mat[0][3] = mat[1][3] = mat[2][3] = 0.0;
+    mat[3][3] = 1.0;
+
     
     glMultMatrixf(mat);
     glTranslatef(-eyeX, -eyeY, -eyeZ);
     
     
 }
+
+
 
 void setUserView(){
     glLoadIdentity( );
@@ -243,15 +289,6 @@ void setUserView(){
     eyeZ = sin(radian_y) * xdistance - cos(radian_y) * sin(radian_x) *
     ydistance + cos(radian_y) * cos(radian_x) * zdistance;
     
-    glulookAt(eyeX, eyeY, eyeZ, atX, atY, atZ, upX, upY, upZ);
+    my_GL_lookAt(eyeX, eyeY, eyeZ, atX, atY, atZ, upX, upY, upZ);
 }
 
-// void setUserView( ){
-//   glLoadIdentity( );
-//   //gluLookAt( 0.0,0.0,distance,0.0,0.0,0.0,0.0,1.0,0.0 );
-//   //angle = angle + 1;
- 
-//   glTranslatef( -xdistance, ydistance, -zdistance );
-//   glRotatef( angle2, 1.0, 0.0, 0.0 );
-//   glRotatef( angle, 0.0, 1.0, 0.0 );
-// }
